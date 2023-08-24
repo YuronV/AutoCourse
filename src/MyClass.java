7728b7c3d13e8ae9b330e2d90b0be138f9b0a93d
Yurii Vashchyshyn <yuriivashchyshyn@yurko.local>
2023-08-24 20:22:53 +0300

    Initial commit

---
+++ src/MyClass.java
@@ -0,0 +1,10 @@
+class myClass {
+    int Year = 2023;
+    public static int MYMETHOD(int a,
+                               int b){
+        int c = a + b; //sum of 2 numbers
+        return c;}
+    public static void main(String[]
+                                    args) {
+        MYMETHOD(num1, num2);}
+}
