8079c839a260134324d2b6c7ff60c126e8118031
yuriivashchyshyn <yura.vashchyshyn@gmail.com>
2023-08-28 18:57:25 +0300

    Code review fixes

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,13 +1,10 @@
-class MyClass {
+public class MyClass {
 
-    public static int myMethod(int a, int b) {
+    public static int sumOfTwoNumbers(int a, int b) {
         return a + b;
     }
 
     public static void main(String[] args) {
-        int num1 = 5;
-        int num2 = 7;
-        int result = myMethod(num1, num2);
-        System.out.println("Result: " + result);
+        System.out.println(sumOfTwoNumbers(5, 7));
     }
 }
